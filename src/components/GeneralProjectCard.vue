f8cd2b21d539827bcf601a46b1fd0a1936916966
Max Prettyjohns <heshoots9999@gmail.com>
2017-08-17 17:32:13 +0100

    Add general project page

---
+++ src/components/GeneralProjectCard.vue
@@ -0,0 +1,163 @@
+<template>
+  <div>
+    <div class="header" v-on:click="openRepo">
+      <div class="projectName">{{ project.name }}</div>
+    </div>
+    <div v-if="!open" class="body">
+    <div>{{display.repo}}</div>
+    <div>{{display.trello}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectCard from './ProjectCard'
+import TrelloCard from './TrelloCard'
+import ContributorCard from './ContributorCard'
+import MetricCard from './MetricCard'
+import Project from '../models/CustomProject'
+
+export default {
+  name: 'general',
+  components: {
+    'projectcard': ProjectCard,
+    'trellocard': TrelloCard,
+    'contributorcard': ContributorCard,
+    'metriccard': MetricCard
+  },
+  props: ['trello', 'users', 'repos', 'project'],
+  data () {
+    return {
+      sources: { name: 'New Project' },
+      contributors: [],
+      settingView: true,
+      display: {
+        trello: [],
+        repo: []
+      },
+      trelloTransforms: {},
+      trellometrics: {},
+      repometrics: {},
+      metrics: []
+    }
+  },
+  mounted () {
+    this.project = new Project(this.project)
+    this.project.fetch()
+      .then((out) => {
+        console.log(out)
+        this.display.trello = out.trello
+        this.display.repo = out.repo
+      })
+  },
+  computed: {
+    displayedTrello () {
+      return this.project.trello.reduce((all, elem) => Array.concat(all, Object.keys(elem)), [])
+    },
+    displayedRepos () {
+      const git = this.project.git.map((repo) => repo.name)
+      const cb = this.project.cb.map((repo) => repo.name)
+      return git.concat(cb)
+    },
+    allRepos () {
+      return this.repos
+    }
+  },
+  watch: {
+    repos () {
+      this.metrics = this.allRepos.reduce((all, elem) => {
+        return all.concat(elem.metrics)
+      })
+    }
+  },
+  methods: {
+    updateTrello () {
+      const updatedDisplay = this.trello.reduce((all, elem) => {
+        if (this.displayedTrello.includes(elem.shortLink)) {
+          if (this.trelloTransforms.hasOwnProperty(elem.shortLink)) {
+            console.log('true')
+            all.push(this.applyTransform(elem, this.trelloTransforms[elem.shortLink].transform))
+          } else {
+            console.log('false')
+            all.push(elem)
+          }
+        }
+        return all
+      }, [])
+      this.display.trello = updatedDisplay
+    },
+    applyTransform (object, transform) {
+      const newObject = object
+      if (transform) {
+        if (transform.merge) {
+          let val = 0
+          if (newObject.metrics[transform.name]) {
+            val = newObject.metrics[transform.name]
+          }
+          transform.merge.map((property) => {
+            if (newObject.metrics.hasOwnProperty(property)) {
+              val += newObject.metrics[property]
+              delete newObject.metrics[property]
+            }
+          })
+          newObject.metrics[transform.name] = val
+        }
+        if (transform.show) {
+          const newMetrics = {}
+          transform.show.map((property) => {
+            if (newObject.metrics.hasOwnProperty(property)) {
+              newMetrics[property] = newObject.metrics[property]
+            }
+          })
+          newObject.metrics = newMetrics
+        }
+      }
+      return newObject
+    }
+  }
+}
+</script>
+
+<style>
+.title {
+  font-size: 40px;
+}
+
+.sources {
+  display: inline;
+  height-height: 400px;
+}
+
+#sources {
+  display: flex;
+  float: none;
+  max-height: 400px;
+}
+
+.avatar {
+  width: 50px;
+  float: right;
+}
+
+.contributors {
+  height: 50px;
+  width: 100%;
+  background-color: #dddddd;
+}
+
+#settings {
+  float: right;
+  height: 50px;
+}
+
+#allSources > div{
+  padding: 10px;
+}
+
+.metrics {
+  display: flex;
+  flex: auto;
+  padding-left: 10px;
+}
+
+</style>
---
+++ src/components/GraphCard.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <div class="header" v-on:click="openRepo">
+      <img class="source" v-bind:class="project.source"></img>
+      <div class="projectName">{{ project.name }}</div>
+      <div v-if="filtered" v-on:click="emitUnfilter">X</div>
+      <div v-if="!small" class="avatarbox">
+        <img class="avatar" v-for="login in project.avatars" v-bind:title="login" v-bind:src="getAvatar(login)"></img>
+      </div>
+    </div>
+    <div v-if="!open" class="body">
+      <div class="bodyleft">
+        {{ project.description }}
+      </div>
+      <div class="bodyright">
+        <ul v-for="key in Object.keys(project.metrics)">
+          <li>{{key}} {{ project.metrics[key] }}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="bottom" v-if="!small" v-bind:class="project.test">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'project',
+  data () {
+    return {
+      closable: this.small,
+      open: this.small
+    }
+  },
+  props: ['project', 'small', 'filtered', 'users'],
+  methods: {
+    openRepo () {
+      if (this.closable) {
+        this.open = !this.open
+      }
+    },
+    emitUnfilter () {
+      this.$emit('unfilter', this.project.shortLink)
+    },
+    getAvatar (login) {
+      const user = this.users.find((user) => {
+        if (user.login === login) {
+          return true
+        }
+        return false
+      })
+      if (user) {
+        if (user.hasOwnProperty('avatar_url')) {
+          return user.avatar_url
+        } else {
+          return ''
+        }
+      } else {
+        return ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .header {
+    background-color: #dddddd;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .body {
+    display: flex;
+  }
+
+  .bodyleft {
+    width: 45%;
+    padding: 10px;
+    background-color: #aaaaaa;
+  }
+
+  .bodyright {
+    width: 55%;
+    padding: 10px;
+    background-color: #888888;
+  }
+
+  .bottom {
+    height: 20px;
+    width: 100%;
+  }
+
+  .notests {
+    background-color: #444455;
+  }
+
+  .testpass {
+    background-color: #449944;
+  }
+
+  .testfail {
+    background-color: #994444;
+  }
+
+  .avatar {
+    width: 50px;
+    float: right;
+    align-items: flex-end;
+  }
+
+  .cb {
+    content:url("https://pbs.twimg.com/profile_images/378800000410745178/8704302b5009e71d66fbc8f52cefc0bc_400x400.png");
+  }
+
+  .git {
+    content:url("https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png");
+    background-color: white;
+  }
+
+  .trello {
+    content:url("http://icons.iconarchive.com/icons/alecive/flatwoken/512/Apps-Trello-icon.png");
+    background-color: white;
+  }
+
+  .source {
+    width: 50px;
+    height: 50px;
+    float: left;
+  }
+
+  .projectName {
+    font-size: 40px;
+    margin: 10px;
+  }
+
+  .avatarBox {
+    align-items: flex-end;
+  }
+</style>
