dfdd67689dc1db2d7f7d230931a9ec5093051f8a
Max Prettyjohns <heshoots9999@gmail.com>
2018-01-23 10:53:51 +0000

    remove appended path

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   //BACKEND: '"https://api-eadms-k8s-local-3jbb00-1671950485.eu-west-1.elb.amazonaws.com/api/v1/proxy/namespaces/default/services/dash-dashboard:8080"',
   //BACKEND: '"http://localhost:4000"',
-  BACKEND: '"http://localhost/api/v1/proxy/namespaces/default/services/dash-dashboard:8080"',
-  ROOT: "/api/v1/proxy/namespaces/default/services/dash-dashboard:8080"
+  BACKEND: '"https://a2c725662002a11e89d870a8fd164a52-506996749.eu-west-1.elb.amazonaws.com"',
+  //ROOT: "/api/v1/proxy/namespaces/default/services/dash-dashboard:8080"
+  ROOT: '""'
 }
